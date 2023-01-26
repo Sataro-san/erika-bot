0b9e748c08550c56ca2436887bfacb59b62dd46e
Placide <brt.aldor@gmail.com>
2023-01-27 02:28:17 +0600

    balance-help info about economistRole

--- src/main/java/kg/shsatarov/erikabot/commands/balance/HelpBalanceCommand.java
+++ src/main/java/kg/shsatarov/erikabot/commands/balance/HelpBalanceCommand.java
@@ -2,6 +2,7 @@ package kg.shsatarov.erikabot.commands.balance;
 
 import kg.shsatarov.erikabot.commands.ExecutableCommand;
 import kg.shsatarov.erikabot.entities.GuildCurrency;
+import kg.shsatarov.erikabot.exceptions.DiscordBotException;
 import kg.shsatarov.erikabot.services.GuildCurrencyService;
 import kg.shsatarov.erikabot.utils.StringFormatter;
 import lombok.RequiredArgsConstructor;
@@ -46,15 +47,19 @@ public class HelpBalanceCommand implements ExecutableCommand {
             return;
         }
 
+        Role economistRole = Optional.ofNullable(slashCommandEvent.getGuild().getRoleById(guildCurrency.getEconomistRoleId()))
+                .orElseThrow(() -> new DiscordBotException("На сервере не установлена роль отвечающая за Экономику :warning:"));
+
 
         slashCommandEvent
                 .reply(StringFormatter.format(
-                        "{}\n:coin: На сервере установлена валюта - ***{}*** ({})\n\n:watch: *Валюта начисляется раз в Минуту при соблюдении следующих условий:*\n  1. У вас должна быть роль ***{}***\n  2. Необходимо включить активности в Discord (чтобы бот мог собирать информацию)\n  3. Статус Онлайн или Не беспокоить\n  4. Нужно быть в голосовом канале, в котором больше одного пользователя\n\n:video_game: За каждую активность (игру) валюта начисляется по разному, для просмотра тарификаций введите команду `/activity-rates`",
+                        "{}\n:coin: На сервере установлена валюта - ***{}*** ({})\n\n:watch: *Валюта начисляется раз в Минуту при соблюдении следующих условий:*\n  1. У вас должна быть роль ***@{}***\n  2. Необходимо включить активности в Discord (чтобы бот мог собирать информацию)\n  3. Статус Онлайн или Не беспокоить\n  4. Нужно быть в голосовом канале, в котором больше одного пользователя\n\nЗа экономику сервера отвечает роль ***@{}***\n\n:video_game: За каждую активность (игру) валюта начисляется по разному, для просмотра тарификаций введите команду `/activity-rates`",
                         slashCommandEvent.getMember().getAsMention(),
                         guildCurrency.getCurrencyName(),
                         guildCurrency.getShortCode(),
-                        currencyRole.getName()
-                        ))
+                        currencyRole.getName(),
+                        economistRole.getName()
+                ))
                 .queue();
 
     }
